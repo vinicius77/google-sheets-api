d4600a39c8d46e794f3463a47e04b6925b6e9445
Vinicius <vinicius.bonifacio1985@gmail.com>
2020-01-20 15:50:02 +0200

    Implements and creates a navbar using Reactstrap

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap';
+
+class AppNavbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar color='dark' dark expand='sm' className='mb-5'>
+          <Container>
+            <NavbarBrand href='/'>Google Sheets API</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className='ml-auto' navbar>
+                <NavItem>
+                  <NavLink href='https://github.com/kako77sub'>Github</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </React.Fragment>
+    );
+  }
+}
+
+export default AppNavbar;
